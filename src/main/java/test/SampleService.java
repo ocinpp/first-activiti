f95b6af5a10f7832e1424f49a8fc42253c24efea
ocinpp <ocin25@gmail.com>
2016-05-12 02:06:01 +0800

    SampleService

---
+++ src/main/java/test/SampleService.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 2016年5月11日
+ *
+ */
+package test;
+
+public class SampleService {
+
+	public String print() {
+		System.out.println("Testing here in SampleService");
+		return "Result in SampleService";
+	}
+	
+}
